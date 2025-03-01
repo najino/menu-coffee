29edd989048bd6bfd580a50f91190033d3896e8d
erfan <erfan.wtf44@gmail.com>
2025-03-01 17:38:01 -0500

    chore: Adding Repository Interface'

---
+++ src/interface/repository.interface.ts
@@ -0,0 +1,14 @@
+import { Document, DeleteResult, Filter, FindCursor, FindOptions, InsertOneResult, OptionalUnlessRequiredId, UpdateResult, WithId } from "mongodb"
+
+export interface IRepo<T extends Document> {
+    create(payload: OptionalUnlessRequiredId<T>): Promise<InsertOneResult<T>>
+
+    update(filter: Filter<T>, paylaod: Partial<T>): Promise<UpdateResult<T>>
+
+    findOne(filter: Filter<T>): Promise<WithId<T> | null>
+
+    findAll(where: Filter<T>, options?: FindOptions): Promise<FindCursor<WithId<T>>>
+
+    delete(filter: Filter<T>): Promise<DeleteResult>
+
+}
