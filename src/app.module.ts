e3d5d81474b2f912993c5ed65bdd96a8c60b6aef
erfan <erfan.wtf44@gmail.com>
2025-03-01 08:32:00 -0500

    chore: Define .env Interface

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
-import { Module } from '@nestjs/common';
-import { ProductModule } from './product/product.module';
-import { UserModule } from './user/user.module';
+import { Module } from "@nestjs/common";
+import { ProductModule } from "./product/product.module";
+import { UserModule } from "./user/user.module";
+import { DatabaseModule } from "./database/database.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [ProductModule, UserModule],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: `${process.cwd()}/.env`,
+			isGlobal: true,
+			cache: true,
+		}),
+		ProductModule,
+		UserModule,
+		DatabaseModule,
+	],
 })
 export class AppModule {}
