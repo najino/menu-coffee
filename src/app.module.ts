0c24cf9b3c1a7ed431894a0d487b763eabf01a06
erfan <erfan.wtf44@gmail.com>
2025-03-01 07:45:33 -0500

    chore: Adding Required Resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule, UserModule],
 })
-export class AppModule { }
+export class AppModule {}
