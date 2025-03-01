f237ae351f20146f2721c10b40dd4a3481b6d450
erfan <erfan.wtf44@gmail.com>
2025-03-01 17:39:15 -0500

    chore: Remover Oldest Interface

--- src/interface/respository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Filter, FindOptions, ObjectId } from "mongodb";
-
-export interface IRepository<T> {
-	create(payload: T): Promise<T>;
-	findOne(id: ObjectId): Promise<T>;
-	find(where?: Filter<T>): Promise<T[]>;
-	update(id: ObjectId, payload: Partial<T>): Promise<T>;
-	delete(id: ObjectId): Promise<T>;
-}
