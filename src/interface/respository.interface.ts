4d77f33715f2f82e3f7fdf9e1f4f139800b7e526
erfan <erfan.wtf44@gmail.com>
2025-03-01 08:50:12 -0500

    chore: Adding Repository Interface

--- src/interface/respository.interface.ts
+++ src/interface/respository.interface.ts
@@ -1,9 +1,9 @@
-import { FindOptions, ObjectId } from "mongodb";
+import { Filter, FindOptions, ObjectId } from "mongodb";
 
 export interface IRepository<T> {
 	create(payload: T): Promise<T>;
 	findOne(id: ObjectId): Promise<T>;
-	find(where?: FindOptions): Promise<T[]>;
+	find(where?: Filter<T>): Promise<T[]>;
 	update(id: ObjectId, payload: Partial<T>): Promise<T>;
 	delete(id: ObjectId): Promise<T>;
 }
