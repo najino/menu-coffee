d03c258d9c563f819be6e213501347897531be80
erfan <erfan.wtf44@gmail.com>
2025-03-02 11:05:11 -0500

    chore: Adding User Controller and Service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { LoginUserDto } from './dtos/login-user.dto';
+import { IsAuth } from '../decorator/auth.decorator';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
+
+  @Post()
+  @IsAuth()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto)
+  }
+
+  @Post('login')
+  login(@Body() loginDto: LoginUserDto) {
+    return this.userService.login(loginDto)
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, HttpException, Injectable, InternalServerErrorException, Logger, } from '@nestjs/common';
 import { UserRepository } from './user.repository';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { LoginUserDto } from './dtos/login-user.dto';
+import { genSalt, hash } from 'bcryptjs';
 
 @Injectable()
 export class UserService {
+    private logger = new Logger(UserService.name);
     constructor(private readonly userRepository: UserRepository) { }
+
+
+    async createUser({ password, username }: CreateUserDto) {
+        try {
+            // check username is unique or not
+            const isUserNameUnique = await this.userRepository.findOne({ username });
+            if (isUserNameUnique)
+                throw new ConflictException("username is exsist before.")
+
+            // hashPassword
+            const salt = await genSalt()
+            const hashPass = await hash(password, salt)
+
+            // insert 
+            const { insertedId } = await this.userRepository.create({ password: hashPass, username })
+
+            return {
+                msg: "user created successfully",
+                id: insertedId
+            };
+
+        } catch (err) {
+            if (err instanceof HttpException)
+                throw err;
+
+            this.logger.error(err)
+            throw new InternalServerErrorException(err)
+        }
+    }
+
+
+    login(credential: LoginUserDto) {
+        return "user Login "
+    }
+
 }
