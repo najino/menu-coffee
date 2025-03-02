1fd8dbd22f0b8e8aa66a4de94e4b59263f503d50
erfan <erfan.wtf44@gmail.com>
2025-03-02 09:45:41 -0500

    refactor: Adding Session Config Into Application

--- src/config/session.config.ts
+++ src/config/session.config.ts
@@ -1,7 +1,26 @@
 import { SessionOptions } from "express-session";
+const session = require('express-session')
+import * as sessionStore from 'connect-mongodb-session';
+import { IEnv } from "../interface/env.interface";
+import { INestApplication } from "@nestjs/common";
 
-export const sessionOption: SessionOptions = {
-    secret: process.env.SECRET || "some",
-    resave: false,
-    saveUninitialized: true,
+
+export function CreateSession(app: INestApplication) {
+    // store Session Data into MongoDB
+    const MongoDbStore = sessionStore(session)
+
+    const store = new MongoDbStore({
+        collection: "sessions",
+        uri: (process.env as unknown as IEnv).MONGO_URI
+    })
+
+    const sessionOption: SessionOptions = {
+        secret: process.env.SECRET || "some",
+        resave: false,
+        saveUninitialized: true,
+        store,
+    }
+
+
+    app.use(session(sessionOption))
 }
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import session from 'express-session';
-import { sessionOption } from './config/session.config';
+const session = require('express-session');
+import { ValidationPipe } from '@nestjs/common';
+import { CreateSession } from './config/session.config';
 
 async function bootstrap() {
   // init Application
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
 
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   // setup session config
-  app.use(
-    session(sessionOption),
-  );
-
+  CreateSession(app)
   // lestening to Project
   await app.listen(port, () => {
     console.log(`server listening on http://localhost:${port}`);
