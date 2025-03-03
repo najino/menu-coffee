a572625bbcdfa332353d8050dd939cbdff6b8cd1
erfan <erfan.wtf44@gmail.com>
2025-03-03 00:30:21 -0500

    chore: Adding Register User Test

--- src/user/test/user.service.spec.ts
+++ src/user/test/user.service.spec.ts
@@ -1,10 +1,13 @@
 import { Test } from "@nestjs/testing"
 import { UserService } from "../user.service"
 import { UserRepository } from "../user.repository"
-
+import { ObjectId } from "mongodb"
+import { ConflictException } from "@nestjs/common"
+import * as bcrypt from 'bcryptjs'
 
 
 describe("User Service Testing", () => {
+
     let userRepositoryMock: Partial<UserRepository> = {
         create: jest.fn(),
         delete: jest.fn(),
@@ -12,6 +15,7 @@ describe("User Service Testing", () => {
         findOne: jest.fn(),
         update: jest.fn()
     }
+
     let service: UserService;
 
     beforeEach(async () => {
@@ -33,4 +37,36 @@ describe("User Service Testing", () => {
         expect(service).toBeDefined()
         expect(userRepositoryMock).toBeDefined()
     })
+
+
+    describe("Register User", () => {
+        it("should be thorw an Error because username is exsist before.", () => {
+            jest.spyOn(userRepositoryMock, 'findOne').mockResolvedValueOnce({ _id: "id" as unknown as ObjectId, username: "test", password: "testPass" })
+
+
+            const promise = service.createUser({ username: "test", 'password': "12341234" })
+
+            expect(promise).rejects.toThrow(ConflictException)
+            expect(promise).rejects.toThrow("username is exsist before.")
+        })
+
+        it("shoulds be registered user", async () => {
+            jest.spyOn(userRepositoryMock, 'findOne').mockResolvedValueOnce(null)
+            jest.spyOn(userRepositoryMock, 'create').mockResolvedValueOnce({ acknowledged: true, insertedId: "id" as unknown as ObjectId })
+            jest.spyOn(bcrypt, 'hash').mockResolvedValueOnce("hash-pass" as never)
+            jest.spyOn(bcrypt, 'genSalt').mockResolvedValueOnce("new-salt" as never)
+
+            const promise = service.createUser({ password: "new-pass", username: "new-username" })
+
+            await expect(promise).resolves.toEqual({ msg: "user created successfully", id: "id" })
+
+            expect(bcrypt.hash).toHaveBeenCalled()
+            expect(bcrypt.hash).toHaveBeenCalledWith("new-pass", 'new-salt')
+            expect(userRepositoryMock.create).toHaveBeenCalledWith({
+                password: "hash-pass",
+                username: "new-username"
+            })
+        })
+    })
+    describe("Login User", () => { })
 })
