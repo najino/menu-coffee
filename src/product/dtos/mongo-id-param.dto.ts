f7273a311fceb86bf850bfeaef6e5a663f62dc96
erfan <erfan.wtf44@gmail.com>
2025-03-04 05:07:35 -0500

    chore: Adding MongoId Dto

---
+++ src/product/dtos/mongo-id-param.dto.ts
@@ -0,0 +1,8 @@
+import { IsMongoId, IsNotEmpty } from "class-validator";
+import { ObjectId } from "mongodb";
+
+export class MongoIdDto {
+    @IsNotEmpty({ message: "please enter Id" })
+    @IsMongoId({ message: "id is invalid" })
+    id: ObjectId
+}
