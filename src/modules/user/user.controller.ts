0fb8a2ca2a125ab0dda79e8b399b41e3d55a0817
erfan <erfan.wtf44@gmail.com>
2025-05-10 18:28:01 -0400

    refactor: Removing Swagger Configuration

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -9,19 +9,8 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { LoginUserDto } from './dtos/login-user.dto';
-import { IsAuth } from '../decorator/auth.decorator';
-import {
-  ApiBearerAuth,
-  ApiBody,
-  ApiConflictResponse,
-  ApiCreatedResponse,
-  ApiNotFoundResponse,
-  ApiOkResponse,
-  ApiOperation,
-  ApiTags,
-  ApiUnauthorizedResponse,
-} from '@nestjs/swagger';
 import { Request } from 'express';
+import { IsAuth } from '../common/decorator/auth.decorator';
 
 @Controller('users')
 export class UserController {
@@ -29,38 +18,11 @@ export class UserController {
 
   @Post()
   @IsAuth()
-  @ApiOperation({ summary: "Register new User" })
-  @ApiTags('Admin')
-  @ApiBearerAuth('JWT-AUTH')
-  @ApiCreatedResponse({
-    description: 'user Created successfully',
-    schema: {
-      type: 'object',
-      properties: {
-        msg: { type: 'string' },
-        accessToken: { type: 'string', description: 'AccessToken' },
-      },
-    },
-  })
-  @ApiUnauthorizedResponse({ description: "user can't access to this route" })
-  @ApiConflictResponse({ description: 'username is exist before.' })
-  @ApiBody({ type: CreateUserDto })
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
   }
 
   @Post('login')
-  @ApiOperation({ summary: "Get AccessToken" })
-  @ApiNotFoundResponse({ description: 'credential are invalid' })
-  @ApiOkResponse({
-    description: 'user login successfully',
-    schema: {
-      type: 'object',
-      properties: {
-        msg: { type: 'string' },
-      },
-    },
-  })
   @HttpCode(HttpStatus.OK)
   async login(@Body() loginDto: LoginUserDto, @Req() req: Request) {
     return this.userService.login(loginDto);
