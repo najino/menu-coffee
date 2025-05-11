9da36eca050ea07966d943ae0ffbbeac11c586c3
erfan <erfan.wtf44@gmail.com>
2025-05-11 13:00:53 -0400

    chore: Adding Category Service

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,28 +1,44 @@
-import { Controller, Delete, Get, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { IsAuth } from "../common/decorator/auth.decorator";
+import { CreateCategoryDto } from "./dto/create-category.dto";
+import { CategoryService } from "./category.service";
+import { UpdateCategoryDto } from "./dto/update-category.dto";
 
 @Controller("categories")
 export class CategoryController {
 
+    constructor(
+        private readonly categoryService:CategoryService
+    ){}
     @Post()
     @IsAuth()
-    createCategory() { }
+    createCategory(@Body() createCategoryDto:CreateCategoryDto) {
+        return this.createCategory(createCategoryDto);
+    }
 
 
     @Get(":slug")
-    getCategoryBySlug() { }
+    getCategoryBySlug(@Param("slug") slug:string) {
+        return this.categoryService.findBySlug(slug);
+    }
 
 
     @Get()
-    getCategories() { }
+    getCategories() {
+        return this.categoryService.findAll();
+    }
 
 
     @Patch(":id")
     @IsAuth()
-    updateCategory() { }
+    updateCategory(@Param("id")categoryId:string,@Body() updateCategoryDto:UpdateCategoryDto) {
+        return this.categoryService.update(categoryId,updateCategoryDto);
+    }
 
 
     @Delete(":id")
     @IsAuth()
-    deleteCategory() { }
+    deleteCategory(@Param("id")categoryId:string) {
+        return this.categoryService.remove(categoryId);
+    }
 }
--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
+import { CategoryRepository } from "./category.repository";
+import { CreateCategoryDto } from "./dto/create-category.dto";
+import { ObjectId } from "mongodb";
+import { UpdateCategoryDto } from "./dto/update-category.dto";
 
 @Injectable()
-export class CategoryService { }
+export class CategoryService {
+
+    constructor(private readonly categoryRepository: CategoryRepository) { }
+    async create(categoryData: CreateCategoryDto) {
+        const category = await this.categoryRepository.findBySlug(categoryData.slug)
+        if(category)
+            throw new BadRequestException("Category Is Exist Before.")
+
+        return this.categoryRepository.create(categoryData);
+    }
+
+    async findAll() {
+        return this.categoryRepository.findAll();
+    }
+
+    async findBySlug(slug: string) {
+        return this.categoryRepository.findBySlug(slug);
+    }
+
+
+    async update(id: string, categoryData: UpdateCategoryDto) {
+        const category = await this.categoryRepository.findOne({_id:new ObjectId(id)});
+        if(!category)
+            throw new NotFoundException("Category Not Found");
+
+        return this.categoryRepository.update(new ObjectId(id), categoryData);
+    }
+
+    async remove(id: string) {
+        const category = await this.categoryRepository.findOne({_id:new ObjectId(id)});
+
+        if(!category)
+            throw new NotFoundException("Category Not Found");
+
+        return this.categoryRepository.delete({_id:new ObjectId(id)});
+    }
+}
